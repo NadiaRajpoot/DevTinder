c806f6e715b5f09a172648323b5bb8f149f6d123
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2025-02-02 12:37:03 -0800

    eploring http methods

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ const express = require("express"); //requirng express.js
 const app = new express(); //creating an instance of express
 
 //defining routes
-app.use("/test" ,(req , res)=>{
-  res.send("hello from server");
+app.use("/user",(req,res)=>{
+  //adding data
+  res.send("I am a use call")
 })
-app.use("/hello" ,(req , res)=>{
-  res.send("hello hello hello");
+app.get("/user" , (req,res)=>{
+  res.send({name: "nadiyee" , surname : "rajpoot"});
 })
-app.use("/about" ,(req , res)=>{
-  res.send("hello this is devTinder");
+
+app.post("/user",(req,res)=>{
+  //adding data
+  res.send("data added successfully")
+})
+app.delete("/user",(req,res)=>{
+  //data deleted
+   res.send("data deleted successfully");
+})
+
+app.patch("/user" ,(req,res)=>{
+  //data updated
+  res.send("data updated successfully")
 })
 
 app.listen(4444 , ()=>{
