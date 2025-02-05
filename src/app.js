7b05f60aa9d0db0ed6ea787fce9ad978a62f717f
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2025-02-04 23:42:39 -0800

    explore wld card error handler

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ const express = require("express"); //requirng express.js
 const app = new express(); //creating an instance of express
 const adminAuth = require("./middlewares/AdminAuth");
 
-app.get("/admin/getData", adminAuth, (req, res) => {
-  console.log("getting admin Data");
-  res.send("getData");
-});
+
 app.get("/admin/deleteData", (req, res) => {
-  console.log("deleting admin data");
-  res.send("delete Data");
+try{
+ res.send("there is no error in this middleware")
+} catch(err){
+  res.status(500).send(err.message)
+}
 });
+
+app.get("/admin/getData", (req, res) => {
+  throw new error();
+  res.status(500).send(err.message)
+}
+);
+app.use("/", (err ,req, res,next) => {
+  if(err)
+    res.status(500).send("the wildcard error")
+});
+
+
 
 app.listen(4444, () => {
   console.log("server running successfully");
