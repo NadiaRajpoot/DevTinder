9df8be11868b5440f90de66aa49befdc33b4b484
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2025-02-06 13:18:45 -0800

    created some http apis

--- src/app.js
+++ src/app.js
@@ -1,7 +1,59 @@
 const express = require("express"); //requirng express.js
 const app = new express(); //creating an instance of express
-
+const User = require("./models/user");
 const connectDB = require("./config/database");
+
+app.use(express.json());
+
+//adding user to the dataBase
+// app.post("/signup", async (req, res) => {
+//   const user = new User(req.body);
+//   try {
+//     await user.save();
+//     res.send("user added successfully");
+//   } catch (err) {
+//     res.status(400).send(err.message);
+//   }
+// });
+
+// //getting user to the database
+// app.get("/user" , async(req , res)=>{
+//   try{
+//     const user = await User.findOne({emailId: req.body.emailId});
+//     res.send(user);
+//   }
+//   catch(err){
+//     res.status(400).send(`error: ${err.message}`);
+//   }
+// })
+
+// getting all users
+// app.get("/user", async (req, res) => {
+//   try {
+//     const users = await User.find();
+
+//     if (!users) {
+//       res.status(404).send("user not found");
+//     } else {
+//       res.send(users);
+//     }
+//   } catch (err) {
+//     res.status(400).send(`error: ${err.message}`);
+//   }
+// });
+
+//getting count of users with specific property
+app.get("/user/:firstName", async (req, res) => {
+  console.log(req.params.firstName)
+  try {
+    const count = await User.countDocuments({ firstName: req.params.firstName });
+
+    res.json({ count }); // ✅ Always send as JSON
+  } catch (err) {
+    res.status(400).json({ error: "wrong" });
+  }
+});
+
 
 connectDB()
   .then(() => {
@@ -11,5 +63,5 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.log(err.message);
+    console.log(`error: ${err.message}`);
   });
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async()=>{
-   await mongoose.connect("mongodb+srv://nadiarajpoot44:678yd2X0pLJ2Qdm8@cluster0.ncmyx.mongodb.net/");
+   await mongoose.connect("mongodb+srv://nadiarajpoot44:678yd2X0pLJ2Qdm8@cluster0.ncmyx.mongodb.net/devTinder");
   
 }
 
