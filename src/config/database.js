655f0401011100f7d1a081f533b8202dc8c2ef05
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2025-12-14 10:12:09 +0000

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async()=>{
-   await mongoose.connect("mongodb+srv://nadiarajpoot44:678yd2X0pLJ2Qdm8@cluster0.ncmyx.mongodb.net/devTinder");
+   await mongoose.connect("mongodb+srv://nadiarajpoot44_db_user:drn9FhmHj7EEGcsz@cluster0.pl1qrrw.mongodb.net/devTinder");
   
 }
 
